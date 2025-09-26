5a782a9ed4389fae38df9e3f6d9592da32ce1d55
775843mk12 <rsmy91806@gmail.com>
2025-09-27 02:15:46 +0300

    Feature; Added working Dark Mode toggle with JS and fixed footer position

---
+++ script.js
@@ -0,0 +1,12 @@
+hemeToggle.addEventListene('click',function() {
+    body.classList.toggle('dark-mode');
+    if
+    (body.classList.contains('dark-mode'))
+    {
+     themeToggle.textContent =   
+    }
+else {
+    themeToggle.texContent =
+}
+
+});
